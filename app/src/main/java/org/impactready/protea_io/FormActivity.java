cc666b616331adfa7c5ef6d083bdbc984fe1dd10
Raoul de Villiers <raouldevilliers@gmail.com>
2015-10-23 14:47:30 +0200

    Changed theme to remove menu and also color now like rest of branding.

--- app/src/main/java/org/impactready/protea_io/FormActivity.java
+++ app/src/main/java/org/impactready/protea_io/FormActivity.java
@@ -27,13 +27,13 @@ public class FormActivity extends AppCompatActivity {
         bundle.putString("type", text );
 
         if (text.equals(getString(R.string.event_main_name))) {
-            getSupportActionBar().setTitle("New " + getString(R.string.event_main_name));
+            getSupportActionBar().setTitle("protea.io : New " + getString(R.string.event_main_name));
 
         } else if (text.equals(getString(R.string.story_main_name))) {
-            getSupportActionBar().setTitle("New " + getString(R.string.story_main_name));
+            getSupportActionBar().setTitle("protea.io :  New " + getString(R.string.story_main_name));
 
         } else if (text.equals(getString(R.string.measurement_main_name))) {
-            getSupportActionBar().setTitle("New " + getString(R.string.measurement_main_name));
+            getSupportActionBar().setTitle("protea.io : New " + getString(R.string.measurement_main_name));
         }
 
         FormActivityFragment fragment = new FormActivityFragment();
@@ -43,27 +43,27 @@ public class FormActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        Log.d(TAG, "Menu inflated..");
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        Log.d(TAG, "Menu inflated..");
+//        return true;
+//    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
 }
